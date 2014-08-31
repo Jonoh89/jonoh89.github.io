a4d3479987e3bdf8eab1aa6c5b9f7d432b5e4bb7
Jonathan Holmlund <jonoh89@gmail.com>
2014-08-31 21:59:01 +0100

    working on theme

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,15 @@ var paths = {
 };
 
 gulp.task('compass', function() {
-    return gulp.src('assets/_scss/*.scss')
+    return gulp.src(paths.scss)
         .pipe(compass({
             project: path.join(__dirname, 'assets'),
             css: 'css',
             sass: '_scss'
         }))
         .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scss, ['compass']);
 });
